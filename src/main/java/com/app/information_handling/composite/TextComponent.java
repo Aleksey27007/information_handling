a53800e3b6cbc32d74e61e0ea4f4d12d744b1f8f
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-02 02:32:34 +0300

    added new methods

--- src/main/java/com/app/information_handling/composite/TextComponent.java
+++ src/main/java/com/app/information_handling/composite/TextComponent.java
@@ -3,10 +3,18 @@ package com.app.information_handling.composite;
 import java.util.List;
 
 public abstract class TextComponent {
-
+    
     public abstract String getText();
-
-    public void print() {
-        throw new UnsupportedOperationException();
+    
+    public void add(TextComponent component) {
+        throw new UnsupportedOperationException("Метод add не поддерживается для этого компонента");
+    }
+    
+    public void remove(TextComponent component) {
+        throw new UnsupportedOperationException("Метод remove не поддерживается для этого компонента");
+    }
+    
+    public List<TextComponent> getChildren() {
+        throw new UnsupportedOperationException("Метод getChildren не поддерживается для этого компонента");
     }
 }
