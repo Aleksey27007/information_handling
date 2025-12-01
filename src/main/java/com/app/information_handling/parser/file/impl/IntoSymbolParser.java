4706c013d0901ec8e0af488dcdb6a9df94d866eb
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-01 19:14:52 +0300

    added symbol regex

---
+++ src/main/java/com/app/information_handling/parser/file/impl/IntoSymbolParser.java
@@ -0,0 +1,14 @@
+package com.app.information_handling.parser.file.impl;
+
+import com.app.information_handling.parser.file.FileParser;
+
+import java.util.List;
+
+public class IntoSymbolParser implements FileParser {
+    private final String SYMBOL_REGEX = "";
+
+    @Override
+    public List<String> parse(String text) {
+        return null;
+    }
+}
