aab45f3ac0b884167e4c47960f54b46fb9b6a2b0
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-01 22:11:56 +0300

    added symbol parser

---
+++ src/main/java/com/app/information_handling/parser/impl/IntoSymbolParserImpl.java
@@ -0,0 +1,33 @@
+package com.app.information_handling.parser.impl;
+
+import com.app.information_handling.parser.FileParser;
+import com.app.information_handling.parser.Regex;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class IntoSymbolParserImpl implements FileParser {
+    private int countSymbol;
+    private List<String> symbols;
+    @Override
+    public List<String> parse(String text) {
+        symbols = Arrays.stream(text.split(Regex.SYMBOL_REGEX.getRegex())).toList(); // делает с табуляциями
+        return symbols;
+    }
+
+    public int getCountSymbol() {
+        return countSymbol;
+    }
+
+    public void setCountSymbol(int countSymbol) {
+        this.countSymbol = countSymbol;
+    }
+
+    public List<String> getSymbols() {
+        return symbols;
+    }
+
+    public void setSymbols(List<String> symbols) {
+        this.symbols = symbols;
+    }
+}
