9ad16e169474e49eac5b69cff1d8f65db2fe6929
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-15 18:54:38 +0300

    finished text component

--- src/main/java/com/app/information_handling/composite/TextComponent.java
+++ src/main/java/com/app/information_handling/composite/TextComponent.java
@@ -3,9 +3,8 @@ package com.app.information_handling.composite;
 import java.util.List;
 
 public interface TextComponent {
-    
     void add(TextComponent textComponent);
     void remove(TextComponent textComponent);
-    List<TextComponent> getComponents;
-    String toString;
+    List<TextComponent> getComponents();
+    String toString();
 }
