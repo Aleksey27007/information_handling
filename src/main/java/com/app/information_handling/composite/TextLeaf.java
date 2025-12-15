3688e30240a2435b120759a2b007f0a2f98b0ccd
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-15 18:54:03 +0300

    finished leaf

---
+++ src/main/java/com/app/information_handling/composite/TextLeaf.java
@@ -0,0 +1,40 @@
+package com.app.information_handling.composite;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TextLeaf implements TextComponent {
+    private static final Logger logger = LogManager.getLogger();
+    private final char symbol;
+    private final TextComponentType type;
+
+    public TextLeaf(char symbol, TextComponentType type) {
+        this.symbol = symbol;
+        this.type = type;
+    }
+
+    @Override
+    public void add(TextComponent textComponent) {
+        logger.error("Leaf can't contain children");
+        throw new UnsupportedOperationException("Leaf can't contain children");
+    }
+
+    @Override
+    public void remove(TextComponent textComponent) {
+        logger.error("Leaf can't contain children");
+        throw new UnsupportedOperationException("Leaf can't contain children");
+    }
+
+    @Override
+    public List<TextComponent> getComponents() {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(symbol);
+    }
+}
