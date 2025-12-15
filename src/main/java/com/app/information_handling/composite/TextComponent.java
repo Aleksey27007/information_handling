c10667004b073b5f866d0350eae04a02332632e9
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-15 18:44:25 +0300

    added interface TextComponent

--- src/main/java/com/app/information_handling/composite/TextComponent.java
+++ src/main/java/com/app/information_handling/composite/TextComponent.java
@@ -2,19 +2,10 @@ package com.app.information_handling.composite;
 
 import java.util.List;
 
-public abstract class TextComponent {
+public interface TextComponent {
     
-    public abstract String getText();
-    
-    public void add(TextComponent component) {
-        throw new UnsupportedOperationException("Method add is not supported for this component");
-    }
-    
-    public void remove(TextComponent component) {
-        throw new UnsupportedOperationException("Method remove is not supported for this component");
-    }
-    
-    public List<TextComponent> getChildren() {
-        throw new UnsupportedOperationException("Method getChildren is not supported for this component");
-    }
+    void add(TextComponent textComponent);
+    void remove(TextComponent textComponent);
+    List<TextComponent> getComponents;
+    String toString;
 }
