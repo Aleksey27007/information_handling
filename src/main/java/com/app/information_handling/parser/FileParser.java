bd6c1d1261a954d8c5d6457269440ecc1ed62a26
Aleksey27007 <alekseyvaruj@gmail.com>
2025-12-16 15:54:04 +0300

    old files deleted

--- src/main/java/com/app/information_handling/parser/FileParser.java
+++
@@ -1,9 +0,0 @@
-package com.app.information_handling.parser;
-
-import java.util.List;
-
-public interface FileParser {
-    List<String> parse(String text);
-    FileParser getNextParser();
-    void setNextParser(FileParser nextParser);
-}
--- src/main/java/com/app/information_handling/service/TextService.java
+++
@@ -1,8 +0,0 @@
-package com.app.information_handling.service;
-
-import com.app.information_handling.composite.Text;
-
-public interface TextService {
-    Text parseText(String text);
-    String restoreText(Text text);
-}
